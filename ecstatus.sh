73535a28a7ad0d2e1fbda0e7a894b5e49f587bf0
Pravina Bhatt <pravina.bhatt@hotmail.com>
2020-07-20 20:53:52 -0400

    Updating checker script

--- ecstatus.sh
+++ ecstatus.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl 'http://ec2-52-15-100-77.us-east-2.compute.amazonaws.com'
+url='http://ec2-52-15-100-77.us-east-2.compute.amazonaws.com'
+data=$(curl "$url")
+status_code=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$url")
+
+if [ "$status_code" -eq "200" ]; then
+echo -e "Nginx browser response status_code is:$status_code \nBrowser data:$data"
+else
+echo "Error status_code: $status_code"
+exit 0
+fi
